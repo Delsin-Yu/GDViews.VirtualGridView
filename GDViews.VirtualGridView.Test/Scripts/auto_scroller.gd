440586f075f0988140a0cc767ebf34752f224196
DE-YU_H14 <delsin_yu@qq.com>
2024-04-21 04:08:50 +0800

    Fix broken auto_scroller

--- GDViews.VirtualGridView.Test/Scripts/auto_scroller.gd
+++ GDViews.VirtualGridView.Test/Scripts/auto_scroller.gd
@@ -10,43 +10,26 @@ var v_tween : Tween;
 var h_scroll_bar : HScrollBar;
 var v_scroll_bar : VScrollBar;
 
-var cached_h_max := NAN;
-var cached_v_max := NAN;
-
 func _ready() -> void:
 	h_scroll_bar = get_h_scroll_bar();
 	v_scroll_bar = get_v_scroll_bar();
-	connect("sort_children", update_tween_deferred);
-
-func update_tween_deferred() -> void:
-	call_deferred("update_tween");
-	
-func update_tween() -> void:
-	_h();
-	_v();
+	v_scroll_bar.changed.connect(_v)
+	h_scroll_bar.changed.connect(_h)
 
 func _h() -> void:
-	var content_max := h_scroll_bar.max_value;
-	if(cached_h_max == content_max): return
-	cached_h_max = content_max;
-	var current_max = size.x;
-	var scroll_duration = (content_max - current_max) / scroll_speed;
-	h_tween = _reset_tween(h_tween, content_max, current_max, h_scroll_bar, scroll_duration);
+	var content_max := h_scroll_bar.max_value - h_scroll_bar.page;
+	h_tween = _reset_tween(h_tween, content_max, h_scroll_bar);
 	pass
 	
 func _v() -> void:
-	var content_max := v_scroll_bar.max_value;
-	if(cached_v_max == content_max): return
-	cached_v_max = content_max;
-	var current_max = size.y;
-	var scroll_duration = (content_max - current_max) / scroll_speed;
-	v_tween = _reset_tween(v_tween, content_max, current_max, v_scroll_bar, scroll_duration);
+	var content_max := v_scroll_bar.max_value - v_scroll_bar.page;
+	v_tween = _reset_tween(v_tween, content_max, v_scroll_bar);
 	pass
 
-func _reset_tween(tween:Tween, content_max:float, current_max:float, scroll_bar:ScrollBar, scroll_duration:float) -> Tween:
+func _reset_tween(tween:Tween, content_max:float, scroll_bar:ScrollBar) -> Tween:
 	if(tween != null): tween.kill();
-	if(current_max >= content_max): return;
 	scroll_bar.value = 0;
+	var scroll_duration := (scroll_bar.max_value - scroll_bar.page) / scroll_speed;
 	var newTween = create_tween();
 	newTween.tween_property(scroll_bar, "value", content_max, scroll_duration).set_delay(stay_seconds);
 	newTween.tween_property(scroll_bar, "value", 0, scroll_duration).set_delay(stay_seconds);
